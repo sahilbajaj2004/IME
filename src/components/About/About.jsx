3280e0572741e68891e9721fbb0aaf8ff25c7095
Sahil bajaj <sahilbajaj0941@gmail.com>
2025-05-27 13:51:57 +0530

    flex-wrap: wrap;

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './About.css'
 import about from '../../assets/about.jpg'
-import play_icon from '../../assets/play-icon.png'
+// import play_icon from '../../assets/play-icon.png'
 
 const About = () => {
   return (
     <div className='about'>
         <div className="about-left">
             <img src={about} alt="" className='about-img'/>
-            <img src={play_icon} alt="" className='play-icon' />
+            {/* <img src={play_icon} alt="" className='play-icon' /> */}
         </div>
         <div className="about-right">
             <h3>About Our Campus</h3>
